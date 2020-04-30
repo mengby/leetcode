7616b8a9091c868960128dca5bea988bc76c999f
mengby <bingyemeng@gmail.com>
2020-04-30 11:13:05 +0800

    add: merge sort

--- src/com/stu/MySort.java
+++ src/com/stu/MySort.java
@@ -1,5 +1,7 @@
 package com.stu;
 
+import java.util.Arrays;
+
 /**
  * Created by bingye on 2020/4/29
  */
@@ -84,15 +86,72 @@ public class MySort {
         return array;
     }
 
+
+//    public static int[] shellSort(int[] array) {
+//        int len = array.length;
+//        int temp , gap = len/2;
+//
+//        while (gap > 0) {
+//            for (int i = 0; i < len; i++) {
+//                temp = array[i];
+//                int  preIndex = i - gap;
+//                while (preIndex >= 0 && array[preIndex] > temp) {
+//                    array[preIndex + gap] = array[preIndex];
+//                    preIndex -= gap;
+//                }
+//                array[preIndex + gap] = temp;
+//            }
+//
+//            gap /= 2;
+//        }
+//
+//        return array;
+//    }
+
+    public static int[] mergeSort(int[] array) {
+        if (array.length < 2) return array;
+        int mid = array.length / 2;
+        int[] left = Arrays.copyOfRange(array, 0, mid);
+        int[] right = Arrays.copyOfRange(array, mid, array.length);
+        return merge(mergeSort(left), mergeSort(right));
+    }
+
+    private static int[] merge(int[] left, int[] right) {
+        int[] result = new int[left.length + right.length];
+
+        for (int index = 0, i = 0, j = 0; index < result.length; index++) {
+            if (i >= left.length)
+                result[index] = right[j++];
+            else if (j >= right.length)
+                result[index] = left[i++];
+            else if (left[i] > right[j])
+                result[index] = right[j++];
+            else
+                result[index] = left[i++];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] array = {2, 1, 5, 3, 9, 6};
 
 //        int[] ints = bubbleSort(array);
 //        int[] ints = selectionSort(array);
-        int[] ints = insertionSort(array);
+//        int[] ints = insertionSort(array);
+//        int[] ints = mergeSort(array);¬
 
-        for (int i = 0; i < ints.length; i++) {
-            System.out.println(ints[i]);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length; j++) {
+                int temp = array[j];
+                if (array[j] < array[j - 1]) {
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
         }
     }
 }
